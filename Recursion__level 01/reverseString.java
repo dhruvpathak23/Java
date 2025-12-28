531179d4a2de2651b1a18920ad83075f6023683f
Dhruv <dhruvkaushik31@gmail.com>
2025-12-28 17:28:52 +0530

    Add files via upload

---
+++ Recursion__level 01/reverseString.java
@@ -0,0 +1,14 @@
+package Recursion.level_01;
+
+public class reverseString {
+    public static String reverse(String str){
+        if(str.isEmpty()){
+            return str;
+        } else {
+            return reverse(str.substring(1)) + str.charAt(0);
+        }
+    }
+    public static void main(String []args){
+        System.out.println(reverse("Mass"));
+    }
+}
