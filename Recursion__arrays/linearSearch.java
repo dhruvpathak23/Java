c17755fed7a77c47fdc311d950352a35b5d79519
Dhruv <dhruvkaushik31@gmail.com>
2025-12-28 17:30:23 +0530

    Add files via upload

---
+++ Recursion__arrays/linearSearch.java
@@ -0,0 +1,16 @@
+package Recursion.arrays;
+// the target element is 18 and the array is [3,2,1,18,45]
+public class linearSearch{
+    static boolean search(int[]arr,int index,int target){
+        if(index == arr.length-1){
+            return false;
+        }
+        return arr[index ]== target || search(arr,index+1,target);
+    }
+    public static void main(String[]args){
+        int []arr = {3,2,1,18,45};
+        int target = 18;
+        System.out.println(search(arr, 0, target));
+    }
+}
+
