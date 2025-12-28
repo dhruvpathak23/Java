f8513b52a22e73559769087b29226d479f902924
Dhruv <dhruvkaushik31@gmail.com>
2025-12-28 19:04:40 +0530

    Update arrayList.java

--- Recursion/arrays/arrayList.java
+++ Recursion/arrays/arrayList.java
@@ -1,1 +1,21 @@
 package Recursion.arrays;
+import java.util.ArrayList;
+// the return type should be in arraylist form 
+// the target element is 4 and the array is [1,3,4,5,4,67,8,87,4] 
+// return all the indices where the target element is found
+public class arrayList {
+    static ArrayList <Integer> solution (int[]arr,int target,int index,ArrayList <Integer>list){
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return solution(arr,target,index+1,list);
+    }
+    public static void main(String[]args){
+        int[] arr = {1,3,4,5,4,67,8,87,4};
+        int target = 4;
+        System.out.println(solution(arr, target, 0, new ArrayList<>()));
+    }
+}
