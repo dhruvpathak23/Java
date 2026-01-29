696195facb314251e9486e95711cf79e5971cbff
Dhruv <dhruvkaushik31@gmail.com>
2026-01-29 16:10:36 +0530

    Add main class to demonstrate Human object creation
    Implement main class to demonstrate Human class usage.

---
+++ OOPS/Static Example/main.java
@@ -0,0 +1,26 @@
+//importing Human class into main class
+import oopslec2.staticExample.Human;
+public class main {
+    public static void main (String[] args){
+
+
+        //creating human objects
+        Human Dhruv = new Human(21, "Dhruv Pathak",90000, false);
+        Human Nitin = new Human(26, "Nitin Pathak",80000, true);
+
+        //print names
+        System.out.println(Dhruv.name);
+        
+
+        //print all data of human objects
+        System.out.println(Nitin);
+
+        //print population
+        System.out.println(Human.population); // correct way to access static variable
+        System.out.println(Dhruv.population); // not recommended way but valid
+       
+        //we know that something which is not static,belongs to an  object
+        //and something which is static,belongs to class
+
+    }
+}
